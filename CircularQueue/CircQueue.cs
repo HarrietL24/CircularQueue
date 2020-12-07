e847abf1c1cdfa0d11876f6cb2f27c014a91f5de
Harriet Leplar <hdlpink2004@outlook.com>
2020-12-07 16:51:18 +0000

    Creating initial skeleton for CircQueue, not done yet

---
+++ CircularQueue/CircQueue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircularQueue
+{
+    class CircQueue
+    {
+        int[] cqueue;
+        int front = 0;
+        int end = 0;
+        int max;
+        int count = 0;
+
+        public CircQueue(int size)
+        {
+            max = size;
+            cqueue = new int[max];
+
+        }
+
+        public void CircEnQueue(int item)
+        {
+            if (end < max)
+            {
+                cqueue[end] = item;
+                end++;
+            }
+            else
+            {
+                throw new Exception("QUEUE FULL!");
+            }
+
+        }
+
+        public int CircDeQueue()
+        {
+            if ()
+            {
+
+            }
+            else
+            {
+                throw new Exception("CIRCULAR QUEUE EMPTY!");
+            }
+        }
+
+
+
+
+
+
+
+    }
+
+    
+}
---
+++ CircularQueue/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CircularQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CircQueue c = new CircQueue(10);
+
+        }
+    }
+}
